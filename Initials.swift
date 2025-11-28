0b178a9d17b02384d155b0292d29cad8d4096e81
ipontes16 <ines.trindade.abreu@gmail.com>
2025-11-28 12:05:37 +0000

    Add initial implementation of initials display

---
+++ Initials.swift
@@ -0,0 +1,9 @@
+//Ines Abreu
+//I played violin for 8 years
+print("IIIII    A  ")
+print("  I     A A ")
+print("  I    A   A")
+print("  I    AAAAA")
+print("  I    A   A")
+print("  I    A   A")
+print("IIIII  A   A")
